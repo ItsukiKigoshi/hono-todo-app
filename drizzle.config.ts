e062f40554bd4a3db29ab750c80e6eb9871bb4ef
Itsuki Kigoshi <itsukikigoshi@gmail.com>
2026-03-17 02:43:18 -0700

    feat: add better-sqlite3@12.8.0 as DevDependencies for Drizzle Kit (Studio) - Studio不要ならbetter-sqlite3も要らない

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
     schema: './src/schema.ts',
     out: './migrations',
     dialect: 'sqlite',
-    driver: 'd1-http',
+    /*driver: 'd1-http',*/
     dbCredentials: {
         url: '.wrangler/state/v3/d1/miniflare-D1DatabaseObject/local.sqlite',
         /*accountId: process.env.CLOUDFLARE_ACCOUNT_ID!,
